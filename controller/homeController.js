cdd6a2cce533042e260a0b5b4f364bed33b99f36
Muad'Dib <135186285+ByteMaster-Y@users.noreply.github.com>
2024-11-17 17:04:39 +0900

    리다이렉션 수정 커밋

--- controller/homeController.js
+++ controller/homeController.js
@@ -8,9 +8,7 @@ const home = async (req, res) => {
             // 최근 게시글 가져오기
             // const recentPosts = await boardModel.getRecentPosts(5); // 최신 5개 게시글 가져오기
             res.render('index', { loginUserInfo }); // recentPosts를 템플릿에 전달
-        } else {
-            res.redirect('/member/login'); // 로그인하지 않은 경우 로그인 페이지로 리디렉션
-        }
+        } 
     } catch (error) {
         res.status(500).send("<H1>500</H1> Error: " + error);
     }
